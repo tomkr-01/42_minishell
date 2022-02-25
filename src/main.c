db401edaf26a80ffcab37c6bbe181d747b9edac6
Rilind Jasari <rjasari@1-B-6.42heilbronn.de>
2022-02-25 14:21:42 +0100

    main.c created

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include "../inc/minishell.h"
+
+int	main(int argc, char *argv[], char **envp)
+{
+	char	*line;
+
+	// function to get the environment into own variable and check if it fails
+	while (1)
+	{
+		signal(SIGQUIT, SIG_IGN);
+		/* sighandler for ctrl C -> display the prompt on a new line */
+	}
+	return (0);
+}
