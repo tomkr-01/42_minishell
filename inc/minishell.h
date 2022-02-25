4b42f045c56d461e817a73192748c620132617d6
Tom Kruger <tkruger@1-B-8.42heilbronn.de>
2022-02-25 13:46:14 +0100

    changes with environment

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tkruger <tkruger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 20:32:44 by tkruger           #+#    #+#             */
-/*   Updated: 2022/02/25 11:25:38 by tkruger          ###   ########.fr       */
+/*   Updated: 2022/02/25 13:44:11 by tkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@
 
 /* minishell.c */
 
-int	main(void);
+int	main(int argc, char **argv, char **envp);
 
 /* lexer.c */
 
--- src/minishell.c
+++ src/minishell.c
@@ -18,58 +18,20 @@
 
 char **g_env;
 
-void	environment_init(void)
+void	environment_init(char **envp)
 {
-	g_env = malloc(5 * sizeof(*g_env));
+	int	i;
+
+	i = 0;
+	g_env = ft_calloc(ft_arrlen(envp), sizeof(*g_env));
 	if (g_env == NULL)
-		exit(ft_putendl_fd("environment_init: env malloc error", STDOUT_FILENO));
-	g_env[0] = ft_strjoin("USER=", getenv("USER"));
-	g_env[1] = ft_strjoin("PATH=", getenv("PATH"));
-	g_env[2] = ft_strjoin("HOME=", getenv("HOME"));
-	g_env[3] = ft_strjoin("PWD=", getenv("PWD"));
-	g_env[4] = ft_strjoin("OLDPWD=", getenv("OLDPWD"));
-	g_env[5] = NULL;
-}
-
-void	environment_export(char *export_var)
-{
-	int		i;
-	char	**new_env;
-
-	i = 0;
-	new_env = ft_calloc(ft_arrlen(g_env) + 1, sizeof(*g_env));
-	while (g_env[i] != NULL)
+		exit(ft_putendl_fd("environment_init(): env ft_calloc error", STDOUT_FILENO));
+	while (envp[i] != NULL)
 	{
-		new_env[i] = g_env[i];
+		g_env[i] = ft_strdup(envp[i]);
 		i++;
 	}
-	new_env[i] = export_var;
-	new_env[i + 1] = NULL;
-	free(g_env);
-	g_env = new_env;
-	new_env = NULL;
-}
-
-void	environment_unset(char *unset_var)
-{
-	int		i;
-	int		j;
-	char	**new_env;
-
-	i = 0;
-	j = 0;
-	new_env = ft_calloc(ft_arrlen(g_env) - 1, sizeof(*g_env));
-	new_env[ft_arrlen(g_env)] = NULL;
-	while (g_env[i] != NULL)
-	{
-		if (ft_strncmp(g_env[i], unset_var, ft_strlen(unset_var)) != 0)
-			new_env[j] = g_env[i];
-		else
-			free(g_env[i++]);
-		i++;
-		j++;
-	}
-	
+	g_env[i] = NULL;
 }
 
 void	environment_print(void)
@@ -77,7 +39,7 @@ void	environment_print(void)
 	int	i;
 
 	i = 0;
-	printf("arrlen: %i\n", ft_arrlen(g_env));
+	printf("arrlen: %zu\n", ft_arrlen(g_env));
 	while (g_env[i] != NULL)
 	{
 		printf("%i: ", i);
@@ -86,14 +48,12 @@ void	environment_print(void)
 	}
 }
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	t_list	*tokens;
 	
 	tokens = split_tok("test");
-	environment_init();
-	environment_print();
-	environment_export(ft_strdup("TEST=doesitworktho??"));
+	environment_init(envp);
 	environment_print();
 	return (0);
 }
