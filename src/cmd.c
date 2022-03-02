3a632f52d6419c383d2ca02bc17e730bdf94e80c
Rilind Jasari <rjasari@1-B-6.42heilbronn.de>
2022-03-02 15:21:19 +0100

    please make the parser better

--- src/cmd.c
+++ src/cmd.c
@@ -4,7 +4,7 @@
 #include "../inc/minishell.h"
 #include <stdlib.h>
 
-// to show tom that we have to dup the old content of the array into the new one
+// // to show tom that we have to dup the old content of the array into the new one
 
 // int	main(void)
 // {
@@ -76,79 +76,234 @@
 // 	return (0);
 // }
 
-// char	*find_executable(char *command)
-// {
-// 	int		index;
-// 	char	*path;
-// 	char	*executable;
-// 	char	*absolute_path;
-// 	char	**directories;
+/* possible simple struct for the parser to put information into
+typedef struct l_parser {
+	int					type_flag;
+	char				*string;
+	struct l_parser		*next;
+}
+*/
 
-// 	if (access(command, F_OK | X_OK) == 0)
-// 		return (command);
-// 	path = getenv("PATH");
-// 	if (path == NULL)
-// 		return (command);
-// 	directories = ft_split(path, ':');
-// 	if (directories == NULL)
-// 		return (command);
-// 	index = 0;
-// 	executable = ft_strjoin("/", command);
-// 	while (directories[index] != NULL)
-// 	{
-// 		absolute_path = ft_strjoin(directories[index], executable);
-// 		if (access(absolute_path, F_OK | X_OK) == 0)
-// 		{
-// 			free(command);
-// 			free(executable);
-// 			// free directories
-// 			return (absolute_path);
-// 		}
-// 		free(absolute_path);
-// 		index++;
-// 	}
-// 	free(executable);
-// 	// free directories
-// 	return (command);
-// }
-
-void	execution(t_list *token)
+int	open_in(char *file_name)
 {
+	int		fd;
+
+	fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(file_name);
+		return (-1);
+	}
+	dup2(fd, 0);
+	return (0);
+}
+
+int	heredoc(char *delimiter)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = get_next_line(0);
+		if (ft_strncmp(line, delimiter, ft_strlen(line)) == 0)
+		{
+			free(line);
+			break ;
+		}
+		else
+			ft_putstr_fd(line, 0);
+		free(line);
+	}
+	return (0);
+}
+
+int	open_out(char *file_name)
+{
+	int		fd;
+
+	fd = open(file_name, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (fd < 0)
+	{
+		perror(file_name);
+		return (-1);
+	}
+	dup2(fd, 1);
+	return (0);
+}
+
+int	open_append(char *file_name)
+{
+	int		fd;
+
+	fd = open(file_name, O_CREAT | O_APPEND | O_WRONLY, 0644);
+	if (fd < 0)
+	{
+		perror(file_name);
+		return (-1);
+	}
+	dup2(fd, 1);
+	return (0);
+}
+
+char	**add_array_element(char **old_arr, char *new_el)
+{
+	size_t	i;
+	size_t	arr_size;
+	char	**new_arr;
+
+	i = 0;
+	arr_size = ft_arrlen(old_arr);
+	new_arr = (char **)malloc((arr_size + 2) * sizeof(char *));
+	if (new_arr == NULL)
+	{
+		perror(new_el);
+		exit(0);
+	}
+	while (i < arr_size)
+	{
+		new_arr[i] = ft_strdup(old_arr[i]);
+		// free(old_arr[i]);
+		// old_arr[i] = NULL;
+		i++;
+	}
+	if (old_arr != NULL)
+		free(old_arr);
+	old_arr = NULL;
+	new_arr[i] = ft_strdup(new_el);
+	new_arr[i + 1] = NULL;
+	return (new_arr);
+}
+
+char	*find_executable(char *command);
+
+void	execution(t_list *token, char **envp)
+{
+	int			pip[2];
+	int			pid;
 	size_t		token_len;
 	char		**command;
+	int			std_fd[2];
 
+	std_fd[0] = dup(STDIN_FILENO);
+	std_fd[1] = dup(STDOUT_FILENO);
+	command = NULL;
 	while (token != NULL)
 	{
-		token_len = ft_strlen(token->content);
-		while (token_len != 1 && token->content[0] != '|')
+		// pipe(pip);
+		// token_len = ft_strlen(token->content);
+		// dup2(std_fd[0], 0);
+		// dup2(std_fd[1], 1);
+		while (token != NULL && token->content[0] != '|')
 		{
 			token_len = ft_strlen(token->content);
 			if (ft_strchr(token->content, '<') != NULL)
 			{
 				if (token_len == 1)
-					; // open infile into stdin
+				{
+					token = token->next;
+					open_in(token->content);
+				}
 				else if (token_len == 2)
-					; // call a heredoc
+				{
+					token = token->next;
+					heredoc(token->content);
+				}
 			}
 			else if (ft_strchr(token->content, '>') != NULL)
 			{
 				if (token_len == 1)
-					; // open a truncated file
+				{
+					token = token->next;
+					open_out(token->content);
+				}
 				else if (token_len == 2)
-					; // open in append mode
+				{
+					token = token->next;
+					open_append(token->content);
+				}
 			}
+			else
+				command = add_array_element(command, token->content);
+			token = token->next;
 		}
+		if (token != NULL && token->content[0] == '|')
+			pipe(pip);
+		if (token != NULL)
+			token = token->next;
+		pid = fork();
+		if (pid == 0)
+		{
+			close(pip[0]);
+			dup2(pip[1], 1);
+			command[0] = find_executable(command[0]);
+			execve(command[0], command, envp);
+			printf("command not found\n");
+		}
+		else if (pid > 0)
+		{
+			close(pip[1]);
+			dup2(pip[0], 0);
+			close(0);
+			dup2(std_fd[0], 0);
+			dup2(std_fd[1], 1);
+			// close(0);
+		}
+		free(command);
+		command = NULL;
 	}
+	while (wait(NULL) != -1)
+		continue ;
 }
 
+char	*find_executable(char *command)
+{
+	int		index;
+	char	*path;
+	char	*executable;
+	char	*absolute_path;
+	char	**directories;
 
-// int	main(void)
-// {
-// 	char	*command;
-// 	char	*path;
-// 	char	**directories;
+	if (access(command, F_OK | X_OK) == 0)
+		return (command);
+	// code a ft_getenv function that does the same as getenv.
+	path = getenv("PATH");
+	if (path == NULL)
+		return (command);
+	directories = ft_split(path, ':');
+	if (directories == NULL)
+		return (command);
+	index = 0;
+	executable = ft_strjoin("/", command);
+	while (directories[index] != NULL)
+	{
+		absolute_path = ft_strjoin(directories[index], executable);
+		if (access(absolute_path, F_OK | X_OK) == 0)
+		{
+			free(command);
+			free(executable);
+			// free directories
+			return (absolute_path);
+		}
+		free(absolute_path);
+		index++;
+	}
+	free(executable);
+	// free directories
+	return (command);
+}
 
-// 	path = getenv("PATH");
-// 	printf("%s\n", path);
-// 	return (0);
-// }
+int	main(int argc, char *argv[], char **envp)
+{
+	char	*exe;
+	char	*command[2];
+	t_list	*tokens;
+
+	tokens = (t_list *)lexer("ls < cmd.c > out -l |  wc -l");
+	if (tokens == NULL)
+		return (0);
+	if (syntax_check(tokens))
+	{
+		execution(tokens, envp);
+	}
+	return (0);
+}
--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 // 	index = 0;
 // 	double_quotes = 0;
 // 	single_quotes = 0;
-// 	while (input[index] != '\0')
+// 	while (input[index] != '\0')c
 // 	{
 // 		if (input[index] == '"' || input[index] == "'")
 // 			to_remove = input[index];
@@ -41,30 +41,31 @@ char	*read_input(void)
 	return (line);
 }
 
-int	main(int argc, char *argv[], char **envp)
-{
-	char	*line;
+// int	main(int argc, char *argv[], char **envp)
+// {
+// 	char	*line;
 
-	// function to get the environment into own variable and check if it fails
-	while (1)
-	{
-		signal(SIGQUIT, SIG_IGN);
-		/* sighandler for ctrl C -> display the prompt on a new line */
-		signal(SIGINT, control_c);
-		change_attributes(false);
-		line = read_input();
-		if (line == NULL)
-		{
-			/* this handles the case for ctrl d sending an EOF to the tty;
-			-> line = NULL and therefore we have to quit and print exit on stdout/stderr?
-			*/
-			/* if you run: bash > out it displays exit on the terminal
-				if you run bash 2> out (redirect stderr) the created file is empty
-				maybe check for tty again?
-			*/
-			write(STDERR_FILENO, "exit\n", 5);
-			return (-1);
-		}
-	}
-	return (0);
-}
+// 	// function to get the environment into own variable and check if it fails
+// 	while (1)
+// 	{
+// 		signal(SIGQUIT, SIG_IGN);
+// 		/* sighandler for ctrl C -> display the prompt on a new line */
+// 		signal(SIGINT, control_c);
+// 		change_attributes(false);
+// 		line = read_input();
+// 		if (line == NULL)
+// 		{
+// 			/* this handles the case for ctrl d sending an EOF to the tty;
+// 			-> line = NULL and therefore we have to quit and print exit on stdout/stderr?
+// 			*/
+// 			/* if you run: bash > out it displays exit on the terminal
+// 				if you run bash 2> out (redirect stderr) the created file is empty
+// 				maybe check for tty again?
+// 			*/
+// 			if (isatty(STDERR_FILENO) == 0)
+// 				write(STDERR_FILENO, "exit\n", 5);
+// 			return (-1);
+// 		}
+// 	}
+// 	return (0);
+// }
