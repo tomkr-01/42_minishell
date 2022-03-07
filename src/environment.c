0b32acbc56b808d7e852cc8c6b734e6fd7a2daa0
Rilind Jasari <rjasari@1-B-6.42heilbronn.de>
2022-03-07 14:38:26 +0100

    ft_getenv was chosen

--- src/environment.c
+++ src/environment.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   environment.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomkrueger <tomkrueger@student.42.fr>      +#+  +:+       +#+        */
+/*   By: rjasari <rjasari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 14:20:32 by tkruger           #+#    #+#             */
-/*   Updated: 2022/03/03 02:02:10 by tomkrueger       ###   ########.fr       */
+/*   Updated: 2022/03/07 14:37:36 by rjasari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,4 @@ void	environment_init(char **envp)
 		i++;
 	}
 	g_env[i] = NULL;
-}
-
-char	*get_var(char *var)
-{
-	size_t	i;
-
-	i = 0;
-	if (var == NULL)
-		return (NULL);
-	while (g_env[i] != NULL)
-	{
-		if (ft_strncmp(g_env[i], var, ft_strlen(var)) == 0)
-			return (ft_strdup(ft_strchr(g_env[i], '=') + 1));
-		i++;
-	}
-	return (NULL);
 }
