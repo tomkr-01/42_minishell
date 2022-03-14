87cb6de7e53b567c6807de4424d61c66fac5fcfa
rilind <rilind.jasari@gmx.de>
2022-03-14 10:05:58 +0100

    for organization

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -202,7 +202,10 @@ void	executioner(t_table *table)
 		table = table->next;
 	}
 	while (wait(NULL) != -1)
-		continue ;
+	{
+		dup2(initial_stdout, 1);
+		dup2(initial_stdin, 0);
+	}
 }
 
 // void	executor(t_table *table)
