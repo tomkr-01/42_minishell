811b69b38a53510f4567f767dea6b9310265a624
Rilind Jasari <rjasari@1-A-7.42heilbronn.de>
2022-03-25 10:45:54 +0100

    fixed exit with empty string error message

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -1,10 +1,36 @@
 #include "../inc/minishell.h"
+#include <limits.h>
+
+int	ft_is_numeric(char *arguments)
+{
+	int		index;
+	int		status;
+
+	index = 0;
+	status = 0;
+	if (arguments == NULL)
+		return (1);
+	if (arguments[index] == '\0')
+		status = 1;
+	else if (arguments[index] == '+' || arguments[index] == '-')
+	{
+		if (ft_isdigit(arguments[++index]) == 0)
+			status = 1;
+	}
+	while (arguments[index] != '\0' && status == 0)
+	{
+		if (!ft_isdigit(arguments[index++]) || ft_strlen(arguments) > 19)
+			status = 1;
+	}
+	if (status == 1)
+		return (0);
+	return (1);
+}
 
 int	exit_builtin(char **arguments)
 {
-	if (isatty(STDOUT_FILENO))
-		ft_putendl_fd("exit", STDERR_FILENO);
-	if (ft_arrlen(arguments) > 0 && !ft_isint(arguments[0]))
+	ft_putendl_fd("exit", STDERR_FILENO);
+	if (!ft_is_numeric(arguments[0]))
 	{
 		put_stderr(SHELL, "exit", arguments[0], "numeric argument required");
 		exit((unsigned char)255);
